c30fde6e0e03a6e004d5bfd342879e4103e8971d
awantika99 <awantikachaudhary99@gmail.com>
2021-11-23 16:04:58 +0530

    Promises

---
+++ Promise.js
@@ -0,0 +1,109 @@
+// Dated - 23rd Nov, 2021
+
+// Promises are used to handle async result.
+/*
+How to Handle Multiple Promises
+Apart from the handler methods (.then, .catch, and .finally), 
+there are six static methods available in the Promise API. 
+The first four methods accept an array of promises and run them in PARALLEL.
+Promise.all
+Promise.any
+Promise.allSettled
+Promise.race
+Promise.resolve
+Promise.reject
+*/
+
+
+// Link to follow -> https://www.youtube.com/watch?v=F8xANXY0kaU
+// Article to follow -> https://www.freecodecamp.org/news/javascript-promise-tutorial-how-to-resolve-or-reject-promises-in-js/
+
+const isMeeting=true;
+const getDetails = new Promise((resolve, reject) => {
+    if(isMeeting){
+        const meeting = {
+            name: 'Marketing',
+            location: 'Skype'
+        }
+        // console.log(meeting)
+        resolve(meeting);
+    }
+    else reject(new Error('Not Scheduled.'));
+});
+
+const addCalendar = meeting =>{
+    const data = `The meeting details are ${meeting.name} and location is ${meeting.location}`;
+    return Promise.resolve(data); 
+}
+
+// classic way of dealing with promises
+const consumer = () => {  // consumer function
+    getDetails    
+    .then(addCalendar)
+    .then(
+        result => console.log(result),
+        error => {}
+    )
+}
+
+// consumer();
+
+// async await example but in the following code, error handling is not available.
+// To handle errors, we use try() catch() blocks where catch() takes care of error handling.
+const AsyncConsumer = async () => {
+    const meetingInfo = await getDetails;  // "await getDetails()" is not valid as getDetails is a PROMISE not a method. 
+    const calendar = await addCalendar(meetingInfo);
+    console.log(calendar);
+}
+
+// AsyncConsumer();
+
+const AsyncConsumerTryCatch = async () => {
+    try {
+        const meetingInfo = await getDetails;  // "await getDetails()" is not valid as getDetails is a PROMISE not a method. 
+        const calendar = await addCalendar(meetingInfo);
+        console.log(calendar); 
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// AsyncConsumerTryCatch();
+
+/*
+const isMeeting=true;
+const details = () => {
+    const getDetails = new Promise((resolve, reject) => {
+        if(isMeeting){
+            const meeting = {
+                name: 'Marketing',
+                location: 'Skype'
+            }
+            // console.log(meeting)
+            resolve(meeting);
+        }
+        else reject(new Error('Not Scheduled.'));
+    });
+    return getDetails;
+}
+const addCalendar = details()
+
+const consumer = () => {
+    addCalendar
+    .then(
+        result => console.log(result)
+    )
+}
+
+consumer();
+*/
+
+const promise2 = new Promise(( resolve, reject )=>{
+    setTimeout(()=>{
+        resolve('This is Shivaji - The Cool')
+    }, 5000)
+})
+
+Promise.all([getDetails, promise2]).then(res => console.log(res));
+
+// Promise.race([getDetails, promise2]).then(res => console.log(res));
